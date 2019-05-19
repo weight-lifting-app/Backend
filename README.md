16b5b15462e99fddb9e655436f1a57aa8498dff4
mrsteveson <patrick.steveson727@gmail.com>
2019-05-19 12:32:44 -0500

    updated login/register endpoints

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Our Backend code for our weight lifting journal.
 
 # Patrick Steveson and Austin James
 
+## Endpoints
 
+| Method | Endpoint               | Requires                        | Description                                                             |
+| ------ | ---------------------- | ------------------------------- | ----------------------------------------------------------------------- |
+| POST   | `/api/auth/register/`  | `username`, `password`, `email` | Registers a New User to our database. Creates a token.                  |
+| POST   | `/api/auth/login/`     | `username`, `password`          | Logs a returning user in. Creates a token.                              |
--- authorization/auth-router.js
+++ authorization/auth-router.js
@@ -7,8 +7,47 @@ const jwt = require('jsonwebtoken');
 const db = require('./auth-model.js');
 const secrets = require('./secrets.js');
 
+//Register User
+router.post('/register', (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12); //hash password
+    user.password = hash;
 
+    if(!user.name && !user.password ) {
+        res.status(401).json({ message: "Please provide a username and password for this user."})
+    } else {
+        db.addNew(user)
+        .then(registered => {
+            const token = generateToken(registered)
+            res.status(201).json({registered, token})
+        })
+        .catch(err => {
+            res.status(500).json(err.message)
+        })
+    };
+});
 
+//Login User
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    db.findBy({ username })
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user); //create token
+            res.status(200).json({
+                message: `Welcome, ${user.username}, enjoy LambdaFit.`,
+                token,
+            })
+        } else {
+            res.status(401).json({ message: "Please provide valid credentials."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err.message)
+    })
+});
 
 // Generate Token
 function generateToken(user) {
