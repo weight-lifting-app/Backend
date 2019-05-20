9be7515ec7e3e58a3d373fdf630498c1587a55e3
mrsteveson <patrick.steveson727@gmail.com>
2019-05-20 14:13:00 -0500

    update migration

--- data/migrations/20190520115719_lambdafitv2.js
+++ data/migrations/20190520115719_lambdafitv2.js
@@ -43,6 +43,6 @@ exports.up = function(knex, Promise) {
   
   exports.down = function(knex, Promise) {
     return knex.schema
-      .dropTableIfExists('users')
       .dropTableIfExists('exercises')
+      .dropTableIfExists('users')
   };
