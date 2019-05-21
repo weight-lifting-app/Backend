402c1d4824cda91cf163ba90da7efaff79a08773
AJLambda <apj2277@gmail.com>
2019-05-21 08:37:04 -0700

    initial commit:

--- data/seeds/002-exercises.js
+++ data/seeds/002-exercises.js
@@ -1,28 +1,27 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('exercises').truncate()
-    .then(function () {
+  return knex("exercises")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('exercises').insert([
+      return knex("exercises").insert([
         {
-          name: 'Bench-Press',
+          name: "Bench-Press",
           user_id: 1,
-          body_region: 'Chest',
-          amount_lifted: '185',
-          reps: '8',
-          date: '2/7/2019'
+          body_region: "Chest",
+          amount_lifted: "185",
+          reps: "8",
+          date: "2/7/2019"
         },
 
         {
-          name: 'Dumbbell Bench-Press',
+          name: "Dumbbell Bench-Press",
           user_id: 1,
-          body_region: 'Chest',
-          amount_lifted: '50',
-          reps: '8',
-          date: '2/7/2019'
-        },
-        
+          body_region: "Chest",
+          amount_lifted: "50",
+          reps: "8",
+          date: "2/7/2019"
+        }
       ]);
     });
 };
